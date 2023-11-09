6e2d8dc36eaf172570c8c73d5e5be4e4e9b65596
Scott Lumley <s.lumley92@gmail.com>
2023-11-09 19:49:30 +0000

    remove uneccesary files

--- RemindersHtmx/RemindersHtmx/Data/Reminder.cs
+++ RemindersHtmx/RemindersHtmx/Data/Reminder.cs
@@ -1,4 +1,6 @@
-﻿namespace RemindersHtmx.Data;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RemindersHtmx.Data;
 
 public class Reminder
 {
@@ -6,8 +8,11 @@ public class Reminder
     public string? Name { get; set; }
     public DateTime DateCreated { get; set; }
     public DateTime DateTime { get; set; }
+    [Display(Name = "Snooze")]
     public TimeSpan SnoozeInterval { get; set; }
+    [Display(Name = "ReReminder")]
     public TimeSpan ReReminderInterval { get; set; }
+    [Display(Name = "Days")]
     public uint DaySchedule { get; set; }
     public string? Note { get; set; }
     //public Location? Location { get; set; }
